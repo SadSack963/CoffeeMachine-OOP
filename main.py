e4daeab6cd5a2eb2a20a4ee1c54884eee5e86233
john <john@jpatmore.plus.com>
2020-11-22 15:51:31 +0000

    Final Version 1.0 Removed menu item creation - it's already done in menu.py

--- main.py
+++ main.py
@@ -2,31 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# Create Menu Item Objects
-cappuccino = MenuItem(
-    name="cappuccino",
-    water=250,
-    milk=100,
-    coffee=24,
-    cost=3.0
-)
-
-espresso = MenuItem(
-    name="espresso",
-    water=50,
-    milk=0,
-    coffee=18,
-    cost=1.5
-)
-
-latte = MenuItem(
-    name="latte",
-    water=200,
-    milk=150,
-    coffee=24,
-    cost=2.5
-)
-
 # Create Objects
 menu = Menu()
 coffee_maker = CoffeeMaker()
